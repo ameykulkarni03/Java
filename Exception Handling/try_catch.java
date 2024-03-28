e3516fda047c786a518798edbcc14c4ba8d7d0bf
ameykulkarni03 <112329132+ameykulkarni03@users.noreply.github.com>
2024-03-29 00:54:33 +0530

    Add files via upload

---
+++ Exception Handling/try_catch.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class try_catch {
+    public static void main(String[] args) {
+        int a=5; 
+        int b=0; 
+          try{ 
+            System.out.println(a/b); 
+          } 
+        catch(ArithmeticException e){ 
+          e.printStackTrace(); 
+        } 
+    }
+}
